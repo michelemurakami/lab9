4c03a4006815ba2531742bdb1a4028fb69e3dffd
Michele Murakami <mmurakam@ucsd.edu>
2020-03-04 23:23:32 -0800

    fixed bugs

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -8,18 +8,20 @@ $(document).ready(function() {
 // Function that is called when the document is ready.
 function initializePage() {
     $.get('/lab/today', onServerResponse);
+		console.log(onServerResponse);
 }
 
 // Function that is called when the server responds to the AJAX call
 function onServerResponse(data){
     var labElementId = getLabElementId(data.id);
+		console.log(labElementId);
     var labElement = $(labElementId);
-    labElement.css({'background': 'yellow'});
+	  labElement.css({'background': 'yellow'});
 }
 
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
